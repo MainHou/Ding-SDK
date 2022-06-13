95f43108d5877d9d88a9c42b8e2daba50855dbc0
zhishui <mainhou@qq.com>
2022-06-13 17:58:10 +0800

    👷  fix: test github action

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DingSDk = void 0;
+var DingSDk = function () {
+    var info = "hello,DingDing JavaScript SDK";
+    console.log(info);
+    return info;
+};
+exports.DingSDk = DingSDk;
